f19826f101920b29a1dddb1ae8ebcfc0824280d9
Samuel Genheden <samuel.genheden@gmail.com>
2019-01-07 18:13:35 +0100

    Re-implement solution 5, adding some more utilities

--- haskell/solutions/solution5.hs
+++ haskell/solutions/solution5.hs
@@ -2,40 +2,17 @@ module Solution5 (
     solve5
 ) where
 
-import Solution3
+import Utils (divisible_by, first_factor, is_power_of)
 
-factor_factory :: Int -> [(Int, Int)]
--- Find the largest prime factor for each of the natural numbers from to 2 to n
-factor_factory n = [(x, Solution3.solve3 x) | x <- [2..n]]
-
-factors_of_x :: [(Int, Int)] -> Int -> [Int]
--- Find all the group of largest prime factors that are all equal to x
-factors_of_x list x = [a | (a,b) <- list, b == x]
-
-factors_list :: Int -> [[Int]]
--- Group the largest prime factor for each of the natural numbers from 2 to n by their largest prime factor
-factors_list n =
-    let largest_factors = factor_factory n
-    in [ factors | x <- [2..n], let factors = factors_of_x largest_factors x, not (null factors)]
-
-is_power_of :: Int -> Int -> Bool
--- Determines if there is a number n such that y ^ n = x
-is_power_of x y =
-    let xi = fromIntegral x
-        yi = fromIntegral y
-        log_frac = log xi / log yi
-    in snd (properFraction log_frac) == 0.0
-
-find_factor :: [Int] -> Int
--- Find the largest prime factor from a list of factors
-find_factor (x:[]) = x
-find_factor (x:xs) =
-    let filtered_xs = filter (\xx -> is_power_of xx x) xs
-    in if not (null filtered_xs) then last filtered_xs else x
 
 solve5 :: Int -> Int
-solve5 n = product(map find_factor (factors_list n))
+solve5 n = product(filtered_list)
+    where list = [2..n]
+          first_factors = map first_factor list
+          list_filter (x, y) = is_power_of x y
+          filtered_list = map fst (filter list_filter (zip first_factors list))
 
-even_divisible_by :: Int -> Int -> Bool
+
+all_divisible_by :: Int -> Int -> Bool
 -- Determines if a number is evenly evenly divisble by all numbers from 2 to n
-even_divisible_by x n = all (\y -> mod x y == 0) [y | y <- [2..n]]
+even_divisible_by x n = all (divisible_by x) [2..n]
--- haskell/solutions/utils.hs
+++ haskell/solutions/utils.hs
@@ -1,10 +1,27 @@
 module Utils (
+    divisible_by,
+    first_factor,
+    is_power_of,
     is_prime
 ) where
+
+divisible_by :: Int -> Int -> Bool
+divisible_by x y = mod x y == 0
+
+first_factor :: Int -> Int
+first_factor 1 = 1
+first_factor n = head (filter (divisible_by n) [2..n])
+
+is_power_of :: Int -> Int -> Bool
+-- Determines if there is a number n such that x ^ n = y
+is_power_of x y =
+    let xi = fromIntegral x
+        yi = fromIntegral y
+        log_frac = log yi / log xi
+    in snd (properFraction log_frac) == 0.0
 
 is_prime :: Int -> Bool
 is_prime 1 = True
 is_prime 2 = True
-is_prime n = null (filter divisible_by [2..limit])
+is_prime n = null (filter (divisible_by n) [2..limit])
     where limit = floor (sqrt (fromIntegral n))
-          divisible_by x = mod n x == 0
